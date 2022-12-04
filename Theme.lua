361c8620624551bf8e05bbc6fb1ccef9bb3aecdf
BogaBoga <88998377+BoobhubIsTrash@users.noreply.github.com>
2022-12-04 19:19:28 +0700

    Update Theme.lua

--- Theme.lua
+++ Theme.lua
@@ -29,8 +29,8 @@ local Library = {
     HudRegistry = {};
 
     FontColor = Color3.fromRGB(255, 255, 255);
-    MainColor = Color3.fromRGB(28, 28, 28);
-    BackgroundColor = Color3.fromRGB(20, 20, 20);
+    MainColor = Color3.fromRGB(23, 23, 23);
+    BackgroundColor = Color3.fromRGB(19, 19, 19);
     AccentColor = Color3.fromRGB(255, 0, 0);
     OutlineColor = Color3.fromRGB(50, 50, 50);
 
